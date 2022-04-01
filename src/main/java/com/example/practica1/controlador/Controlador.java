20f8812ade1d6b9ea884bb9723bfe52f8b5fa68d
Paola Gallegos <paolagallegos@ciencias.unam.mx>
2022-03-31 23:25:35 -0600

    Paso 5

---
+++ src/main/java/com/example/practica1/controlador/Controlador.java
@@ -0,0 +1,25 @@
+package com.example.practica1.controlador;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class Controlador{
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @PostMapping("/entrar")
+    public String entrar(HttpServletRequest request, Model modelo){
+        String nombre = request.getParameter("nombre");
+        //System.out.println("nombre ->" + nombre);
+        modelo.addAttribute("nombre", nombre);
+        return "cuenta";
+    }
+}
